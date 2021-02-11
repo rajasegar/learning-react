6fc74472b6be0b13d7fd4608f04f4eb969ee9624
Rajasegar <rajasegar.c@gmail.com>
2021-02-11 09:33:49 +0530

    chore:first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import StarRating from './StarRating.jsx';
 
 function App() {
   // Create the count state.
@@ -13,25 +14,10 @@ function App() {
   // Return the App component.
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <StarRating 
+        style={{backgroundColor: 'lightblue'}}
+        onDoubleClick={e => alert('double click')}
+      />
     </div>
   );
 }
