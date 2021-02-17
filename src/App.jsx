ab1b60c7d6aa60068f659d0e71bc2997491024f2
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-02-17 08:42:44 +0530

    chore: adding state to colors

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,39 @@ import StarRating from './StarRating.jsx';
 import colorData from './color-data.json';
 import ColorList from './ColorList.jsx';
 import AddColorForm from './AddColorForm.jsx';
+import { v4 } from 'uuid';
 
 function App() {
   const [colors, setColors] = useState(colorData);
   return (
     <div className="App">
-      <ColorList 
+      <ColorList
         colors={colors}
         onRateColor={(id, rating) => {
-          const newColors = colors.map(color =>
-            color.id === id ? {...color, rating } : color
+          const newColors = colors.map((color) =>
+            color.id === id ? { ...color, rating } : color,
           );
           setColors(newColors);
         }}
-        onRemoveColor={id => {
-          const newColors = colors.filter(color => color.id !== id );
+        onRemoveColor={(id) => {
+          const newColors = colors.filter((color) => color.id !== id);
           setColors(newColors);
         }}
       />
-          <AddColorForm onNewColor={(title,color) => {
-            const newColors = [...colors, {title, color}];
-            setColors(newColors);
-          }}/>
+      <AddColorForm
+        onNewColor={(title, color) => {
+          const newColors = [
+            ...colors,
+            {
+              id: v4(),
+              rating: 0,
+              title,
+              color,
+            },
+          ];
+          setColors(newColors);
+        }}
+      />
     </div>
   );
 }
